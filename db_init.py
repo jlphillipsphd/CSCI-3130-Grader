163b8f84f4945114125cb0a4fbcdd185bbca7c6e
Ivan <fio2003@gmail.com>
2018-05-04 03:25:05 -0500

    Added scripts to create Grades database and fill students table. Most likely they will be changed once I come up with better design or need in additional fields.

--- db_init.py
+++ db_init.py
@@ -85,8 +85,74 @@ def grades_db_create(db_name, force=False):
     :param force: flag to overwrite existing db
     :return: Unknown
     """
-    pass
+    if os.path.isfile(db_name) and force:
+        with lite.connect(db_name) as con:
+            cur = con.cursor()
+            # TODO: force should remove 'IF NOT EXISTS' and add 'DROP TABLE' to ensure new table creation
+            # WISH: add try blocks for each CREATE and spawn new info window in case of error
+            cur.execute("""CREATE TABLE grades (
+                            id               INTEGER PRIMARY KEY AUTOINCREMENT,
+                            stud_id                  NOT NULL REFERENCES students (id) ON UPDATE CASCADE,
+                            lab                      NOT NULL REFERENCES lab_names (id) ON UPDATE CASCADE,
+                            attempt          INT     DEFAULT (0),
+                            submitted        INTEGER,
+                            graded           INTEGER,
+                            grade            INTEGER NOT NULL DEFAULT (0),
+                            pass_fail        BOOLEAN NOT NULL DEFAULT (FALSE),
+                            grader_comment   TEXT,
+                            extra_comment    TEXT,
+                            report_generated BOOLEAN,
+                            report_time      INTEGER  );""")
 
+            cur.execute("""CREATE TABLE students (
+                            id             TEXT    NOT NULL,
+                            year           INT     NOT NULL,
+                            semester       CHAR    NOT NULL REFERENCES semesters (semester) ON DELETE RESTRICT
+                                                                                            ON UPDATE CASCADE,
+                            first_name     TEXT    NOT NULL,
+                            second_name    TEXT    NOT NULL,
+                            comment        TEXT,
+                            cheating_ratio INTEGER DEFAULT (0),
+                            PRIMARY KEY (id, year, semester)    );""")
+
+            cur.execute("""CREATE TABLE semesters (
+                            semester CHAR (1) NOT NULL PRIMARY KEY,
+                            name     VARCHAR  );""")
+
+            cur.execute("""CREATE TABLE lab_names (
+                            id             INT     NOT NULL PRIMARY KEY,
+                            type           TEXT    NOT NULL,
+                            num            INTEGER NOT NULL,
+                            max_grade      INTEGER NOT NULL,
+                            name           VARCHAR,
+                            description    VARCHAR,
+                            grader_comment VARCHAR  );""")
+            con.commit()
+
+
+def load_student_list_into_grades_db(db_name, year, semester, filename='students_list.txt'):
+    """
+    Imports list of students from file in format: `id % lname, fname` into Grades DB.
+    Should be called before first grading.
+    :param db_name: db that contains grades and student info
+    :param year: grading (current) year
+    :param semester: grading (current) semester
+    :param filename: file that contains student list
+    :return: nothing
+    """
+
+    with open(filename, 'r') as sl:
+        ids, names = zip(*(line.strip().split('%') for line in sl))
+        lname, fname = zip(*(namer.split(',') for namer in names))
+        names_tupple = list(zip(ids, fname, lname, [year]*len(ids), [semester]*len(ids), [0]*len(ids)))
+
+    if os.path.isfile(db_name):
+        with lite.connect(db_name) as con:
+            cur = con.cursor()
+            cur.executemany('INSERT OR REPLACE INTO STUDENTS \
+                        (id, first_name, second_name, year, semester, cheating_ratio) VALUES (?, ?, ?, ?, ?, ?)',
+                        names_tupple)
+            con.commit()
 
 if __name__ == '__main__':
     settings_db_create()
